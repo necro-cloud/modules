673d4263e7d405f3ebdf7c66ff124ffadcbf20ea
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-03-09 12:00:23 +0530

    feat(minio): ingresses for minio and minio api

---
+++ modules/minio/ingress.tf
@@ -0,0 +1,89 @@
+resource "kubernetes_ingress_v1" "ingress" {
+  metadata {
+    name      = "ingress"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    labels = {
+      app       = var.app_name
+      component = "ingress"
+    }
+    annotations = {
+      "nginx.ingress.kubernetes.io/proxy-ssl-verify" : "on"
+      "nginx.ingress.kubernetes.io/proxy-ssl-secret" : "${kubernetes_namespace.namespace.metadata[0].name}/${kubernetes_manifest.internal_certificate.manifest.spec.secretName}"
+      "nginx.ingress.kubernetes.io/proxy-ssl-name" : "minio.minio.svc.cluster.local"
+      "nginx.ingress.kubernetes.io/backend-protocol" : "HTTPS"
+      "nginx.ingress.kubernetes.io/rewrite-target" : "/"
+      "nginx.ingress.kubernetes.io/proxy-body-size" : 0
+    }
+  }
+
+  spec {
+    ingress_class_name = "nginx"
+    tls {
+      hosts       = ["${var.host_name}.${var.domain}"]
+      secret_name = kubernetes_manifest.ingress_certificate.manifest.spec.secretName
+    }
+    rule {
+      host = "${var.host_name}.${var.domain}"
+      http {
+        path {
+          path = "/"
+          backend {
+            service {
+              name = "minio-console"
+              port {
+                number = 9443
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [kubernetes_manifest.minio_tenant]
+}
+
+resource "kubernetes_ingress_v1" "api_ingress" {
+  metadata {
+    name      = "api-ingress"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    labels = {
+      app       = var.app_name
+      component = "ingress"
+    }
+    annotations = {
+      "nginx.ingress.kubernetes.io/proxy-ssl-verify" : "on"
+      "nginx.ingress.kubernetes.io/proxy-ssl-secret" : "${kubernetes_namespace.namespace.metadata[0].name}/${kubernetes_manifest.internal_certificate.manifest.spec.secretName}"
+      "nginx.ingress.kubernetes.io/proxy-ssl-name" : "minio.minio.svc.cluster.local"
+      "nginx.ingress.kubernetes.io/backend-protocol" : "HTTPS"
+      "nginx.ingress.kubernetes.io/rewrite-target" : "/"
+      "nginx.ingress.kubernetes.io/proxy-body-size" : 0
+    }
+  }
+
+  spec {
+    ingress_class_name = "nginx"
+    tls {
+      hosts       = ["api.${var.host_name}.${var.domain}"]
+      secret_name = kubernetes_manifest.api_ingress_certificate.manifest.spec.secretName
+    }
+    rule {
+      host = "api.${var.host_name}.${var.domain}"
+      http {
+        path {
+          path = "/"
+          backend {
+            service {
+              name = "minio"
+              port {
+                number = 443
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  depends_on = [kubernetes_manifest.minio_tenant]
+}
