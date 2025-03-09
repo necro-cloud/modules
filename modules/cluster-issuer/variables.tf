cf3de76f2d01075a16e2fa52b88a6393047b39e5
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-03-09 09:27:29 +0530

    feat(cluster-issuer): helm installation for cert-manager

---
+++ modules/cluster-issuer/variables.tf
@@ -0,0 +1,14 @@
+# -------------- CERT MANAGER HELM CHART VARIABLES -------------- #
+variable "cert_manager_configuration" {
+  description = "Dictionary filled with Cert Manager Operator Configuration Details"
+  type        = map(string)
+  default = {
+    "name"             = "cert-manager"
+    "namespace"        = "cert-manager"
+    "repository"       = "https://charts.jetstack.io"
+    "chart"            = "cert-manager"
+    "version"          = "v1.17.1"
+    "create_namespace" = true
+  }
+}
+
