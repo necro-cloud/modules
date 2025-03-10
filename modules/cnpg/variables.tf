29a040ffc290f03d3bbb360402a59d476fd8f326
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-03-10 22:06:22 +0530

    feat(cnpg): minio, server and client certificates for postgresql

--- modules/cnpg/variables.tf
+++ modules/cnpg/variables.tf
@@ -24,3 +24,58 @@ variable "namespace" {
   default     = "postgres"
 }
 
+variable "minio_namespace" {
+  description = "Namespace for the MinIO Deployment for storing PITR Backups"
+  type        = string
+  nullable    = false
+}
+
+# --------------- CERTIFICATE VARIABLES --------------- #
+variable "minio_certificate_authority" {
+  description = "Name of the Certificate Authority associated with the MinIO Storage Solution"
+  type        = string
+  nullable    = false
+}
+
+variable "cluster_issuer_name" {
+  description = "Name for the Cluster Issuer to be used to generate internal self signed certificates"
+  type        = string
+  nullable    = false
+}
+
+variable "server_certificate_authority_name" {
+  description = "Name of the Certificate Authority to be used with PostgreSQL Server"
+  type        = string
+  default     = "postgresql-server-certificate-authority"
+}
+
+variable "server_issuer_name" {
+  description = "Name of the Issuer to be used with PostgreSQL Server"
+  type        = string
+  default     = "postgresql-server-issuer"
+}
+
+variable "server_certificate_name" {
+  description = "Name of the Certificate to be used with PostgreSQL Server"
+  type        = string
+  default     = "postgresql-server-certificate"
+}
+
+variable "client_certificate_authority_name" {
+  description = "Name of the Certificate Authority to be used with PostgreSQL Client"
+  type        = string
+  default     = "postgresql-client-certificate-authority"
+}
+
+variable "client_issuer_name" {
+  description = "Name of the Issuer to be used with PostgreSQL Client"
+  type        = string
+  default     = "postgresql-client-issuer"
+}
+
+variable "client_streaming_replica_certificate_name" {
+  description = "Name of the Certificate to be used with PostgreSQL Streaming Replica Client"
+  type        = string
+  default     = "postgresql-streaming-replica-client-certificate"
+}
+
