7600357dfe1ff9ac5e8b9f12e13580af7e4fee4b
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-03-31 11:13:40 +0530

    feat(keycloak): updating secret for realm configuration

--- modules/keycloak/secrets.tf
+++ modules/keycloak/secrets.tf
@@ -78,13 +78,16 @@ resource "kubernetes_secret" "realm_secrets" {
   }
 
   data = {
-    DISPLAY_NAME     = var.realm_settings["display_name"]
-    APPLICATION_NAME = var.realm_settings["application_name"]
-    SMTP_HOST        = var.realm_settings["smtp_host"]
-    SMTP_PORT        = var.realm_settings["smtp_port"]
-    SMTP_MAIL        = var.realm_settings["smtp_mail"]
-    SMTP_USERNAME    = var.realm_settings["smtp_username"]
-    SMTP_PASSWORD    = var.realm_settings["smtp_password"]
-    TESTER_SECRET    = random_password.tester_password.result
+    DISPLAY_NAME               = var.realm_settings["display_name"]
+    APPLICATION_NAME           = var.realm_settings["application_name"]
+    BASE_URL                   = var.realm_settings["base_url"]
+    VALID_LOGIN_REDIRECT_PATH  = var.realm_settings["valid_login_redirect_path"]
+    VALID_LOGOUT_REDIRECT_PATH = var.realm_settings["valid_logout_redirect_path"]
+    SMTP_HOST                  = var.realm_settings["smtp_host"]
+    SMTP_PORT                  = var.realm_settings["smtp_port"]
+    SMTP_MAIL                  = var.realm_settings["smtp_mail"]
+    SMTP_USERNAME              = var.realm_settings["smtp_username"]
+    SMTP_PASSWORD              = var.realm_settings["smtp_password"]
+    TESTER_SECRET              = random_password.tester_password.result
   }
 }
