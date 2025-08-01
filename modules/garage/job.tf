088cc3144c5137c314538a342ce28629845b8745
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-08-01 13:07:03 +0530

    feat(garage): configurator job implementation

---
+++ modules/garage/job.tf
@@ -0,0 +1,81 @@
+resource "kubernetes_job" "configurator" {
+  metadata {
+    name      = "garage-configurator"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    labels = {
+      app       = var.app_name
+      component = "job"
+    }
+  }
+
+  spec {
+    template {
+      metadata {
+        labels = {
+          app       = var.app_name
+          component = "pod"
+          created-by : "job"
+        }
+      }
+      spec {
+        service_account_name = kubernetes_service_account.garage_configurator_service_account.metadata[0].name
+        container {
+          name  = "garage-configurator"
+          image = "quay.io/necronizerslab/garage-configurator:0.7.9"
+
+          env {
+            name  = "CONFIGURATOR_JSON"
+            value = "/mnt/config/configurator.json"
+          }
+
+          env {
+            name = "GARAGE_ADMIN_TOKEN"
+            value_from {
+              secret_key_ref {
+                name = kubernetes_secret.admin_password.metadata[0].name
+                key  = "GARAGE_ADMIN_TOKEN"
+              }
+            }
+          }
+
+          env {
+            name  = "NODE_EXTRA_CA_CERTS"
+            value = "/mnt/crts/ca.crt"
+          }
+
+          env {
+            name  = "EXECUTION_MODE"
+            value = "cluster"
+          }
+
+          volume_mount {
+            name       = "garage-ca"
+            mount_path = "/mnt/crts/"
+          }
+
+          volume_mount {
+            name       = "garage-config"
+            mount_path = "/mnt/config"
+          }
+        }
+
+        volume {
+          name = "garage-ca"
+          secret {
+            secret_name = kubernetes_manifest.internal_certificate.manifest.metadata.name
+          }
+        }
+
+        volume {
+          name = "garage-config"
+          config_map {
+            name = kubernetes_config_map.configurator-options.metadata[0].name
+          }
+        }
+      }
+
+    }
+  }
+
+  depends_on = [kubernetes_stateful_set.statefulset]
+}
