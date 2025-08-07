8c5ac56f9464b6515bbce044e0c2d9437d8cc16b
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-08-07 09:07:38 +0530

    feat(garage): configuring outputs for other modules to consume

---
+++ modules/garage/outputs.tf
@@ -0,0 +1,13 @@
+// Garage Namespace Output
+output "garage_namespace" {
+  value       = kubernetes_namespace.namespace.metadata[0].name
+  description = "Namespace where Garage Storage Solution is deployed in"
+  depends_on  = [kubernetes_job.configurator]
+}
+
+// Garage Internal Certificate Name Output
+output "garage_internal_certificate_secret" {
+  value       = kubernetes_manifest.internal_certificate.manifest.spec.secretName
+  description = "Secret name where the Internal Certificate for Garage is stored in"
+  depends_on  = [kubernetes_job.configurator]
+}
