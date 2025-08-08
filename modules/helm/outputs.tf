13c2e00f35493553fc59ecd02070a499d0049e82
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-08-08 12:04:10 +0530

    fix(helm): fixing outputs for minio

--- modules/helm/outputs.tf
+++ modules/helm/outputs.tf
@@ -1,4 +1,4 @@
 output "minio-operator-namespace" {
   description = "Namespace where the MinIO Operator is deployed in"
-  value       = helm_release.minio.namespace
+  value       = helm_release.minio[0].namespace
 }
