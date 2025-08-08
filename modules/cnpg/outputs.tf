4a68891b348b7246553b50509ef5f17a227da17e
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-08-08 11:28:32 +0530

    feat(cnpg): output dependencies

--- modules/cnpg/outputs.tf
+++ modules/cnpg/outputs.tf
@@ -1,9 +1,11 @@
 output "namespace" {
   description = "Namespace where the PostgreSQL Database is deployed in"
   value       = kubernetes_namespace.namespace.metadata[0].name
+  depends_on  = [kubernetes_manifest.databases, kubernetes_manifest.keycloak_database]
 }
 
 output "server-certificate-authority" {
   description = "Certificate Authority being used with PostgreSQL Database"
   value       = kubernetes_manifest.server_certificate_authority.manifest.spec.secretName
+  depends_on  = [kubernetes_manifest.databases, kubernetes_manifest.keycloak_database]
 }
