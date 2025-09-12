abb9e7a64e002aa24e31d4f0f4d0857b67b94cd5
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-09-12 11:38:18 +0530

    feat(valkey): valkey & sentinel configuration with services

---
+++ modules/valkey/configmap.tf
@@ -0,0 +1,57 @@
+resource "kubernetes_config_map" "valkey_conf" {
+  metadata {
+    name      = "valkey-configuration"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    labels = {
+      app       = var.app_name
+      component = "configmap"
+    }
+  }
+  data = {
+    "valkey.conf" = <<EOF
+      port 0
+      tls-port 6379
+
+      protected-mode no
+      appendonly yes
+
+      dir /data
+
+      # This is critical. Replicas need this to auth with a newly promoted primary.
+      primaryauth "$(VALKEY_PASSWORD)"
+
+      # TLS Configuration
+      tls-cert-file /etc/valkey/tls/tls.crt
+      tls-key-file /etc/valkey/tls/tls.key
+      tls-ca-cert-file /etc/valkey/tls/ca.crt
+    EOF
+  }
+}
+
+resource "kubernetes_config_map" "sentinel_conf" {
+  metadata {
+    name      = "sentinel-configuration"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    labels = {
+      app       = var.app_name
+      component = "configmap"
+    }
+  }
+  data = {
+    "sentinel.conf" = <<EOF
+      port 0
+      tls-port 26379
+      
+      sentinel monitor main valkey-primary-service 6379 2
+      sentinel auth-pass main "$(VALKEY_PASSWORD)"
+      sentinel down-after-milliseconds main 5000
+      sentinel failover-timeout main 10000
+      sentinel parallel-syncs main 1
+
+      # TLS Configuration for Sentinel itself.
+      tls-cert-file /etc/valkey/tls/tls.crt
+      tls-key-file /etc/valkey/tls/tls.key
+      tls-ca-cert-file /etc/valkey/tls/ca.crt
+    EOF
+  }
+}
---
+++ modules/valkey/service.tf
@@ -0,0 +1,95 @@
+resource "kubernetes_service" "headless_service" {
+  metadata {
+    name      = "valkey-headless-service"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    labels = {
+      app       = var.app_name
+      component = "service"
+    }
+  }
+
+  spec {
+    type       = "ClusterIP"
+    cluster_ip = null
+
+    port {
+      port        = 6379
+      target_port = 6379
+    }
+
+    selector = {
+      app       = var.app_name
+      "part-of" = "valkey-cluster"
+    }
+  }
+}
+
+resource "kubernetes_service" "primary_service" {
+  metadata {
+    name      = "valkey-primary-service"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    labels = {
+      app       = var.app_name
+      component = "service"
+    }
+  }
+
+  spec {
+    port {
+      port        = 6379
+      target_port = 6379
+    }
+
+    selector = {
+      app                                  = var.app_name
+      "part-of"                            = "valkey-cluster"
+      "statefulset.kubernetes.io/pod-name" = "valkey-cluster-0"
+    }
+  }
+}
+
+resource "kubernetes_service" "replica_service" {
+  metadata {
+    name      = "valkey-replica-service"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    labels = {
+      app       = var.app_name
+      component = "service"
+    }
+  }
+
+  spec {
+    port {
+      port        = 6379
+      target_port = 6379
+    }
+
+    selector = {
+      app       = var.app_name
+      "part-of" = "valkey-cluster"
+    }
+  }
+}
+
+resource "kubernetes_service" "sentinel_service" {
+  metadata {
+    name      = "valkey-sentinel-service"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    labels = {
+      app       = var.app_name
+      component = "service"
+    }
+  }
+
+  spec {
+    port {
+      port        = 26379
+      target_port = 26379
+    }
+
+    selector = {
+      app       = var.app_name
+      "part-of" = "valkey-sentinel"
+    }
+  }
+}
