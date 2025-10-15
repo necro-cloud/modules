fd87e9a10c91268c3ba12b2843ea5029091970d2
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-10-15 12:20:22 +0530

    feat(reflector): reflector updated for #35 , #38

--- modules/helm/reflector.tf
+++ modules/helm/reflector.tf
@@ -6,4 +6,6 @@ resource "helm_release" "reflector" {
   chart            = var.reflector_configuration.chart
   version          = var.reflector_configuration.version
   create_namespace = var.reflector_configuration.create_namespace
+
+  set = [var.node_selector]
 }
