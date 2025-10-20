fbdea9f7b19e10ff6f4fc3dcd922f2ba81474aa4
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-10-20 10:28:54 +0530

    fix(helm): required values in Helm Chart for Calico

--- modules/helm/calico.tf
+++ modules/helm/calico.tf
@@ -1,16 +1,36 @@
-# // Calico Configuration
-# resource "helm_release" "calico" {
-#   name             = var.calico_configuration.name
-#   namespace        = var.calico_configuration.namespace
-#   repository       = var.calico_configuration.repository
-#   chart            = var.calico_configuration.chart
-#   version          = var.calico_configuration.version
-#   create_namespace = var.calico_configuration.create_namespace
+// Calico Configuration
+resource "helm_release" "calico" {
+  name             = var.calico_configuration.name
+  namespace        = var.calico_configuration.namespace
+  repository       = var.calico_configuration.repository
+  chart            = var.calico_configuration.chart
+  version          = var.calico_configuration.version
+  create_namespace = var.calico_configuration.create_namespace
 
-#   set = [
-#     {
-#       name  = "nodeSelector.server"
-#       value = var.server_node_selector
-#     }
-#   ]
-# }
+  set = [
+    {
+      name  = "nodeSelector.server"
+      value = var.server_node_selector
+    },
+    {
+      name  = "installation.calicoNetwork.ipPools[0].cidr"
+      value = "10.42.0.0/16"
+    },
+    {
+      name  = "installation.calicoNetwork.ipPools[0].natOutgoing"
+      value = "Enabled"
+    },
+    {
+      name  = "installation.calicoNetwork.ipPools[0].ipipMode"
+      value = "Always"
+    },
+    {
+      name  = "installation.calicoNetwork.ipPools[0].nodeSelector"
+      value = "all()"
+    },
+    {
+      name  = "installation.calicoNetwork.policy.type"
+      value = "Calico"
+    }
+  ]
+}
