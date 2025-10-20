83e1463517f9124797d42fe63d47e8c55ab7cd9d
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-10-20 09:15:01 +0530

    feat(valkey): pdb defined, for #41

---
+++ modules/valkey/poddisruptionbudget.tf
@@ -0,0 +1,22 @@
+resource "kubernetes_pod_disruption_budget" "valkey_pdb" {
+  metadata {
+    name      = "valkey-cluster-pdb"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    labels = {
+      app       = var.app_name
+      component = "pdb"
+    }
+  }
+
+  spec {
+    min_available = 2
+    selector {
+      match_labels = {
+        app       = var.app_name
+        "part-of" = "valkey-cluster"
+      }
+    }
+  }
+
+  depends_on = [kubernetes_stateful_set.valkey_cluster]
+}
