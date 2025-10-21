ccc3ace17a480f610642ba2207cb50975aa0203f
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-10-21 09:10:06 +0530

    fix(valkey): using native netpols for deployment

--- modules/valkey/networkpolicy.tf
+++ modules/valkey/networkpolicy.tf
@@ -1,72 +1,98 @@
 // Network policy to restrict network access to the Valkey Cluster
-resource "kubernetes_manifest" "valkey_network_policy" {
-  manifest = {
-    "apiVersion" = "projectcalico.org/v3"
-    "kind"       = "NetworkPolicy"
-    "metadata" = {
-      "name"      = "valkey-network-access"
-      "namespace" = kubernetes_namespace.namespace.metadata[0].name
-      "labels" = {
-        "app"       = var.app_name
-        "component" = "networkpolicy"
+resource "kubernetes_network_policy" "valkey_network_access_policy" {
+  metadata {
+    name      = "valkey-network-access-policy"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    labels = {
+      app       = var.app_name
+      component = "networkpolicy"
+    }
+  }
+
+  spec {
+    pod_selector {
+      match_labels = {
+        "app"     = var.app_name
+        "part-of" = "valkey-cluster"
       }
     }
 
-    "spec" = {
-      "selector" = "app == '${var.app_name}' && part-of == 'valkey-cluster'"
-      "types"    = ["Ingress", "Egress"]
+    policy_types = ["Ingress", "Egress"]
 
-      # -------------- INGRESS RULES -------------- #
-      "ingress" = [
-        # Rule 1: Allow Cluster Intercommunication Ingress
-        {
-          "action"   = "Allow"
-          "protocol" = "TCP"
-          "source" = {
-            "selector" = "app == '${var.app_name}' && part-of == 'valkey-cluster'"
-          }
-          "destination" = {
-            "ports" = [6379]
-          }
-        },
-
-        # Rule 2: Allow Network Ingress from allowed namespaces
-        {
-          "action"   = "Allow"
-          "protocol" = "TCP"
-          "source" = {
-            "selector"          = "valkey-access == 'true'"
-            "namespaceSelector" = "kubernetes.io/metadata.name in {${join(", ", formatlist("'%s'", var.replication_namespaces))}}"
-          }
-          "destination" = {
-            "ports" = [6379]
+    # -------------- INGRESS RULES -------------- #
+    # Rule 1: Allow ingress from other Valkey Pods
+    ingress {
+      from {
+        pod_selector {
+          match_labels = {
+            "app"     = var.app_name
+            "part-of" = "valkey-cluster"
           }
         }
-      ]
+      }
+      ports {
+        protocol = "TCP"
+        port     = 6379
+      }
+    }
 
-      # -------------- EGRESS RULES -------------- #
-      "egress" = [
-        # Rule 1: Allow Valkey pods to send traffic to each other.
-        {
-          "action"   = "Allow"
-          "protocol" = "TCP"
-          "destination" = {
-            "selector" = "app == '${var.app_name}' && part-of == 'valkey-cluster'"
-            "ports"    = [6379]
+    # Rule 2: Allow ingress from trusted pods in trusted namespaces
+    ingress {
+      from {
+        namespace_selector {
+          match_expressions {
+            key      = "kubernetes.io/metadata.name"
+            operator = "In"
+            values   = split(",", var.replication_namespaces)
           }
-        },
+        }
+        pod_selector {
+          match_labels = {
+            "valkey-access" = true
+          }
+        }
+      }
+      ports {
+        protocol = "TCP"
+        port     = 6379
+      }
+    }
 
-        # Rule 2: Allow DNS resolution to kube-dns.
-        {
-          "action"   = "Allow"
-          "protocol" = "UDP"
-          "destination" = {
-            "selector"          = "k8s-app == 'kube-dns'"
-            "namespaceSelector" = "kubernetes.io/metadata.name == 'kube-system'"
-            "ports"             = [53]
+    # -------------- INGRESS RULES -------------- #
+    # Rule 1: Allow egress to other Valkey pods
+    egress {
+      to {
+        pod_selector {
+          match_labels = {
+            "app"     = var.app_name
+            "part-of" = "valkey-cluster"
           }
-        },
-      ]
+        }
+      }
+      ports {
+        protocol = "TCP"
+        port     = 6379
+      }
+    }
+
+    # Rule 2: Allow DNS resolution to KubeDNS
+    egress {
+      to {
+        namespace_selector {
+          match_labels = {
+            "kubernetes.io/metadata.name" = "kube-system"
+          }
+        }
+        pod_selector {
+          match_labels = {
+            "k8s-app" = "kube-dns"
+          }
+        }
+      }
+      ports {
+        protocol = "UDP"
+        port     = 53
+      }
     }
   }
 }
