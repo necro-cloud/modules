ae6c157871a6e11bdbe75a46bdb887c23bedad8f
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2025-11-12 11:09:53 +0530

    fix(cnpg): removing affinity and topology

--- modules/cnpg/cluster.tf
+++ modules/cnpg/cluster.tf
@@ -17,34 +17,6 @@ resource "kubernetes_manifest" "cluster" {
           "garage-access" = true
         }
       }
-      "affinity" = {
-        "nodeAffinity" = {
-          "requiredDuringSchedulingIgnoredDuringExecution" = {
-            "nodeSelectorTerms" = [
-              {
-                "matchExpressions" = [
-                  {
-                    "key"      = "worker"
-                    "operator" = "exists"
-                  }
-                ]
-              }
-            ]
-          }
-        }
-      }
-      "topologySpreadConstraints" = [
-        {
-          "maxSkew"           = 1
-          "topologyKey"       = "kubernetes.io/hostname"
-          "whenUnsatisfiable" = "DoNotSchedule"
-          "labelSelector" = {
-            "matchLabels" = {
-              "cnpg.io/cluster" = var.cluster_name
-            }
-          }
-        }
-      ]
       "imageCatalogRef" = {
         "apiGroup" = "postgresql.cnpg.io",
         "kind"     = "ClusterImageCatalog"
