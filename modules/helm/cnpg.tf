ef39967f05298c8ec6764fc000cc189919568aa2
khatrivarun <varun.h.khatri.dev@protonmail.com>
2026-01-01 16:57:30 +0530

    feat(example): trying something with Calico CNI

--- modules/helm/cnpg.tf
+++ modules/helm/cnpg.tf
@@ -1,34 +1,34 @@
-// Cloud Native PG Operator Configuration
-resource "helm_release" "cnpg" {
-  name             = var.cnpg_configuration.name
-  namespace        = var.cnpg_configuration.namespace
-  repository       = var.cnpg_configuration.repository
-  chart            = var.cnpg_configuration.chart
-  version          = var.cnpg_configuration.version
-  create_namespace = var.cnpg_configuration.create_namespace
+# // Cloud Native PG Operator Configuration
+# resource "helm_release" "cnpg" {
+#   name             = var.cnpg_configuration.name
+#   namespace        = var.cnpg_configuration.namespace
+#   repository       = var.cnpg_configuration.repository
+#   chart            = var.cnpg_configuration.chart
+#   version          = var.cnpg_configuration.version
+#   create_namespace = var.cnpg_configuration.create_namespace
 
-  set = [
-    {
-      name  = "nodeSelector.server"
-      value = var.server_node_selector
-    }
-  ]
-}
+#   set = [
+#     {
+#       name  = "nodeSelector.server"
+#       value = var.server_node_selector
+#     }
+#   ]
+# }
 
-// Barman Cloud Plugin installation
-resource "helm_release" "cnpg_barman_plugin" {
-  name       = var.cnpg_barman_configuration.name
-  namespace  = var.cnpg_barman_configuration.namespace
-  repository = var.cnpg_barman_configuration.repository
-  chart      = var.cnpg_barman_configuration.chart
-  version    = var.cnpg_barman_configuration.version
+# // Barman Cloud Plugin installation
+# resource "helm_release" "cnpg_barman_plugin" {
+#   name       = var.cnpg_barman_configuration.name
+#   namespace  = var.cnpg_barman_configuration.namespace
+#   repository = var.cnpg_barman_configuration.repository
+#   chart      = var.cnpg_barman_configuration.chart
+#   version    = var.cnpg_barman_configuration.version
 
-  set = [
-    {
-      name  = "nodeSelector.server"
-      value = var.server_node_selector
-    }
-  ]
+#   set = [
+#     {
+#       name  = "nodeSelector.server"
+#       value = var.server_node_selector
+#     }
+#   ]
 
-  depends_on = [helm_release.cnpg, helm_release.cert-manager]
-}
+#   depends_on = [helm_release.cnpg, helm_release.cert-manager]
+# }
--- modules/helm/reflector.tf
+++ modules/helm/reflector.tf
@@ -1,16 +1,16 @@
-# Kubernetes Reflector Configuration
-resource "helm_release" "reflector" {
-  name             = var.reflector_configuration.name
-  namespace        = var.reflector_configuration.namespace
-  repository       = var.reflector_configuration.repository
-  chart            = var.reflector_configuration.chart
-  version          = var.reflector_configuration.version
-  create_namespace = var.reflector_configuration.create_namespace
+# # Kubernetes Reflector Configuration
+# resource "helm_release" "reflector" {
+#   name             = var.reflector_configuration.name
+#   namespace        = var.reflector_configuration.namespace
+#   repository       = var.reflector_configuration.repository
+#   chart            = var.reflector_configuration.chart
+#   version          = var.reflector_configuration.version
+#   create_namespace = var.reflector_configuration.create_namespace
 
-  set = [
-    {
-      name  = "nodeSelector.server"
-      value = var.server_node_selector
-    }
-  ]
-}
+#   set = [
+#     {
+#       name  = "nodeSelector.server"
+#       value = var.server_node_selector
+#     }
+#   ]
+# }
