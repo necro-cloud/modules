51ef982452aa78bef3d630733c81b1acbd787f66
khatrivarun <varun.h.khatri.dev@protonmail.com>
2026-01-12 11:59:34 +0530

    fix(ferretdb): variable reference fixes

--- modules/ferretdb/outputs.tf
+++ modules/ferretdb/outputs.tf
@@ -1,5 +1,5 @@
 output "namespace" {
   description = "Namespace where the PostgreSQL Database is deployed in"
   value       = kubernetes_namespace.namespace.metadata[0].name
-  depends_on  = [ubernetes_manifest.ferret_database]
+  depends_on  = [kubernetes_manifest.ferret_database]
 }
