5f05b4726ecb8d6ff52f697d88d19393630eea41
khatrivarun <varun.h.khatri.dev@protonmail.com>
2026-01-12 15:23:22 +0530

    feat(ferretdb): ferret pdb implementation

--- modules/ferretdb/poddisruptionbudget.tf
+++ modules/ferretdb/poddisruptionbudget.tf
@@ -19,3 +19,27 @@ resource "kubernetes_pod_disruption_budget_v1" "cnpg_pdb" {
 
   depends_on = [kubernetes_manifest.cluster]
 }
+
+resource "kubernetes_pod_disruption_budget_v1" "ferret_pdb" {
+  metadata {
+    name      = "ferret-pdb"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    labels = {
+      app       = var.app_name
+      component = "pdb"
+    }
+  }
+
+  spec {
+    min_available = 1
+    selector {
+      match_labels = {
+        app = var.app_name
+        component = "pod"
+        "part-of" = "ferretdb"
+      }
+    }
+  }
+
+  depends_on = [kubernetes_deployment.ferretdb]
+}
