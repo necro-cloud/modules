bc3bd989dd785972a9a46f2157d9cdbe9d7e8e6c
khatrivarun <varun.h.khatri.dev@protonmail.com>
2026-01-15 14:25:24 +0530

    feat(ferretdb): service for mongo express

--- modules/ferretdb/service.tf
+++ modules/ferretdb/service.tf
@@ -25,3 +25,31 @@ resource "kubernetes_service" "ferret_service" {
     type = "ClusterIP"
   }
 }
+
+// Mongo Express Service for Ingress Usage
+resource "kubernetes_service" "mongo_express" {
+  metadata {
+    name      = "mongo-express-service"
+    namespace = kubernetes_namespace.namespace.metadata[0].name
+    labels = {
+      app       = var.app_name
+      component = "service"
+    }
+  }
+
+  spec {
+    type = "ClusterIP"
+
+    port {
+      port        = 8081
+      target_port = 8081
+      name        = "https"
+    }
+
+    selector = {
+      app       = var.app_name
+      component = "pod"
+      used-for  = "mongo-express"
+    }
+  }
+}
