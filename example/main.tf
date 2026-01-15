db7fb619be2c19634f17d236e7bb6629ccabc361
khatrivarun <varun.h.khatri.dev@protonmail.com>
2026-01-15 12:47:46 +0530

    feat(ferretdb): remove not required stuff from the variable

--- example/main.tf
+++ example/main.tf
@@ -81,7 +81,7 @@ module "cnpg" {
 
 # FerretDB Deployment for MongoDB Database Solution
 module "ferretdb" {
-  source = "git::https://github.com/necro-cloud/modules//modules/ferretdb?ref=main"
+  source = "git::https://github.com/necro-cloud/modules//modules/ferretdb?ref=task/68/authn-setup"
 
   // Garage Cluster Details for configuration of PITR Backups
   garage_certificate_authority = module.garage.garage_internal_certificate_secret
@@ -94,9 +94,6 @@ module "ferretdb" {
     {
       namespace          = "cloud"
       user               = "cloud"
-      database           = "cloud"
-      derRequired        = false
-      privateKeyEncoding = "PKCS1"
     }
   ]
 
