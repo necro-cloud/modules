707852a9ad81b2c2eb3169f9d0e87929e1945efc
Varun Khatri <varun.h.khatri.dev@protonmail.com>
2026-02-09 10:19:29 +0530

    [TASK] Implementation of deployment of VictoriaMetrics (#79)
    # Implemented Changes
    - Implementation of deployment of VictoriaMetrics
    - This will be used for storing metrics across the cluster
    - Configured to be deployed on agent nodes
    **Closes: #74**

---
+++ modules/observability/metrics.tf
@@ -0,0 +1,57 @@
+resource "helm_release" "metrics" {
+  name = "victoria-metrics"
+  repository = "https://victoriametrics.github.io/helm-charts/"
+  chart = "victoria-metrics-single"
+  version = "0.30.0"
+
+  namespace = kubernetes_namespace.namespace.metadata[0].name
+
+  values = [
+    yamlencode({
+      server = {
+        replicaCount = 1
+        retentionPeriod = "7d"
+
+        resources = {
+          requests = {
+            cpu    = "100m"
+            memory = "256Mi"
+          }
+          limits = {
+            cpu    = "500m"
+            memory = "768Mi"
+          }
+        }
+
+        persistentVolume = {
+          enabled      = true
+          accessModes  = ["ReadWriteOnce"]
+          size         = "10Gi"
+          storageClassName = "local-path"
+        }
+
+        podLabels = {
+          app = var.app_name
+          component = "pod"
+        }
+          
+        affinity =  {
+          nodeAffinity = {
+            requiredDuringSchedulingIgnoredDuringExecution = {
+              nodeSelectorTerms = [
+                {
+                  matchExpressions = [
+                    {
+                      key      = "worker"
+                      operator = "Exists"
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        }
+      }
+    })
+  ]
+}
