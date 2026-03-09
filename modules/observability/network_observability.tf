3bfc134fb7d2e78a7d8fc0786342116c4de06c8c
khatrivarun <varun.h.khatri.dev@protonmail.com>
2026-03-09 12:55:24 +0530

    feat(observability): some comments

--- modules/observability/network_observability.tf
+++ modules/observability/network_observability.tf
@@ -9,23 +9,25 @@ resource "kubernetes_manifest" "network_observability" {
     spec = {
       namespace = kubernetes_namespace.namespace.metadata[0].name
       
-      // "Direct" mode sends logs straight to OTel (bypassing Kafka/IPFIX)
+      // "Direct" mode sends logs straight to OTel
       deploymentModel = "Direct"
 
       agent = {
         type = "eBPF"
         ebpf = {
-          // PRIVILEGED: Required for "PacketDrop" to read kernel drop reasons
+          // Required for "PacketDrop" to read kernel drop reasons
           privileged = true
           
           // Enable drop detection and TCP round trips metrics
           features = ["PacketDrop", "FlowRTT"] 
           
-          // SAMPLING: 25 means 1 in 25 packets
+          // 25 means 1 in 25 packets
           sampling           = 25
           cacheActiveTimeout = "15s"
           cacheMaxFlows      = 100000
-          excludeInterfaces  = ["lo"] // Ignore loopback traffic
+
+          // Ignore loopback traffic
+          excludeInterfaces  = ["lo"]
 
           // Resource Constraints
           resources = {
@@ -34,8 +36,8 @@ resource "kubernetes_manifest" "network_observability" {
               memory = "100Mi"
             }
             limits = {
-              cpu    = "500m"  // Hard cap to prevent eBPF compiler spikes
-              memory = "512Mi" // Hard cap to prevent leaks
+              cpu    = "500m"
+              memory = "512Mi"
             }
           }
         }
@@ -54,23 +56,20 @@ resource "kubernetes_manifest" "network_observability" {
         enable = false
       }
 
-      // PROCESSOR: Enrichment settings
+      //  Enrichment settings
       processor = {
         logTypes = "Flows"
         metrics = {
           // Disable agent-side metrics generation to save CPU
-          // We will derive metrics from logs in Victoria if needed
           disableAlerts = ["NetObservLokiError", "NetObservNoFlows"] 
         }
       }
 
-      // EXPORT: Pushing to the OTel Collector
+      // Pushing metrics to the OTel Collector
       exporters = [
         {
           type = "OpenTelemetry"
           openTelemetry = {
-            // 1. POINT THIS TO YOUR OTEL COLLECTOR SERVICE
-            // Format: <service-name>.<namespace>.svc.cluster.local
             targetHost = "otel-collector.${kubernetes_namespace.namespace.metadata[0].name}.svc.cluster.local"
             targetPort = 4317
             protocol   = "grpc"
