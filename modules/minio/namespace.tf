23169c3f8397fc649162f63059cde8da069a2440
khatrivarun <varun.h.khatri.dev@protonmail.com>
2026-03-09 13:31:41 +0530

    feat(minio): complete module removal

--- modules/minio/namespace.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_namespace" "namespace" {
-  metadata {
-    name = var.namespace
-    labels = {
-      "app"       = var.app_name
-      "component" = "namespace"
-    }
-  }
-}
