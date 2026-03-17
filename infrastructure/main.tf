9ed1c13d3b53b8eae5b627abe4779b00190bb724
khatrivarun <varun.h.khatri.dev@protonmail.com>
2026-03-17 12:32:05 +0530

    fix(openbao): resource timeouts due to bad internet

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -35,7 +35,7 @@ module "observability" {
 
 # OpenBao Secrets Management Solution deployment
 module "openbao" {
-  source = "git::https://github.com/necro-cloud/modules//modules/openbao?ref=main"
+  source = "git::https://github.com/necro-cloud/modules//modules/openbao?ref=task/118/ferret-secrets"
   
   // Certificates Details
   cluster_issuer_name = module.cluster-issuer.cluster-issuer-name
