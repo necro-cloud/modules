d4aa794ff0fc518d2cfe1ae63ea9eff5d0011979
khatrivarun <varun.h.khatri.dev@protonmail.com>
2026-03-19 12:50:43 +0530

    feat(keycloak): shifting all secrets management to ESO and OpenBao

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -91,132 +91,132 @@ module "garage" {
   depends_on = [module.observability, module.openbao]
 }
 
-# # Cloudnative PG Deployment for PostgreSQL Database Solution
-# module "cnpg" {
-#   source = "git::https://github.com/necro-cloud/modules//modules/cnpg?ref=main"
+# Cloudnative PG Deployment for PostgreSQL Database Solution
+module "cnpg" {
+  source = "git::https://github.com/necro-cloud/modules//modules/cnpg?ref=main"
   
-#   // Cluster Secret Store Details
-#   cluster_secret_store_name = module.openbao.cluster_secret_store_name
+  // Cluster Secret Store Details
+  cluster_secret_store_name = module.openbao.cluster_secret_store_name
 
-#   // Garage Cluster Details for configuration of PITR Backups
-#   garage_certificate_authority = module.garage.garage_internal_certificate_secret
-#   garage_namespace             = module.garage.garage_namespace
-#   garage_configuration         = "walbackups"
-#   backup_bucket_name           = "postgresql"
+  // Garage Cluster Details for configuration of PITR Backups
+  garage_certificate_authority = module.garage.garage_internal_certificate_secret
+  garage_namespace             = module.garage.garage_namespace
+  garage_configuration         = "walbackups"
+  backup_bucket_name           = "postgresql"
 
-#   // Observability details
-#   observability_namespace = module.observability.observability_namespace
+  // Observability details
+  observability_namespace = module.observability.observability_namespace
 
-#   // Required client details to allow access and generate credentials and certificates for
-#   clients = [
-#     {
-#       namespace          = "cloud"
-#       user               = "cloud"
-#       database           = "cloud"
-#       derRequired        = false
-#       privateKeyEncoding = "PKCS1"
-#     }
-#   ]
+  // Required client details to allow access and generate credentials and certificates for
+  clients = [
+    {
+      namespace          = "cloud"
+      user               = "cloud"
+      database           = "cloud"
+      derRequired        = false
+      privateKeyEncoding = "PKCS1"
+    }
+  ]
 
-#   // Certificate details for internal and ingress(pgadmin) certificates
-#   cloudflare_token    = var.cloudflare_token
-#   cloudflare_email    = var.cloudflare_email
-#   domain              = var.domain
-#   cluster_issuer_name = module.cluster-issuer.cluster-issuer-name
+  // Certificate details for internal and ingress(pgadmin) certificates
+  cloudflare_token    = var.cloudflare_token
+  cloudflare_email    = var.cloudflare_email
+  domain              = var.domain
+  cluster_issuer_name = module.cluster-issuer.cluster-issuer-name
 
-#   // Whitelisting Kubernetes API Endpoints in the Network Policy
-#   kubernetes_api_ip       = one(flatten(data.kubernetes_endpoints_v1.kubernetes_api_endpoint.subset[*].address[*].ip))
-#   kubernetes_api_protocol = one(flatten(data.kubernetes_endpoints_v1.kubernetes_api_endpoint.subset[*].port[*].protocol))
-#   kubernetes_api_port     = one(flatten(data.kubernetes_endpoints_v1.kubernetes_api_endpoint.subset[*].port[*].port))
+  // Whitelisting Kubernetes API Endpoints in the Network Policy
+  kubernetes_api_ip       = one(flatten(data.kubernetes_endpoints_v1.kubernetes_api_endpoint.subset[*].address[*].ip))
+  kubernetes_api_protocol = one(flatten(data.kubernetes_endpoints_v1.kubernetes_api_endpoint.subset[*].port[*].protocol))
+  kubernetes_api_port     = one(flatten(data.kubernetes_endpoints_v1.kubernetes_api_endpoint.subset[*].port[*].port))
 
-#   // Dependency on Garage Deployment  
-#   depends_on = [module.garage, module.observability, module.openbao]
-# }
+  // Dependency on Garage Deployment  
+  depends_on = [module.garage, module.observability, module.openbao]
+}
 
-# # FerretDB Deployment for MongoDB Database Solution
-# module "ferretdb" {
-#   source = "git::https://github.com/necro-cloud/modules//modules/ferretdb?ref=main"
+# FerretDB Deployment for MongoDB Database Solution
+module "ferretdb" {
+  source = "git::https://github.com/necro-cloud/modules//modules/ferretdb?ref=main"
 
-#   // Cluster Secret Store Details
-#   cluster_secret_store_name = module.openbao.cluster_secret_store_name
+  // Cluster Secret Store Details
+  cluster_secret_store_name = module.openbao.cluster_secret_store_name
 
-#   // Garage Cluster Details for configuration of PITR Backups
-#   garage_certificate_authority = module.garage.garage_internal_certificate_secret
-#   garage_namespace             = module.garage.garage_namespace
-#   garage_configuration         = "walbackups"
-#   backup_bucket_name           = "ferret"
+  // Garage Cluster Details for configuration of PITR Backups
+  garage_certificate_authority = module.garage.garage_internal_certificate_secret
+  garage_namespace             = module.garage.garage_namespace
+  garage_configuration         = "walbackups"
+  backup_bucket_name           = "ferret"
 
-#   // Observability details
-#   observability_namespace = module.observability.observability_namespace
+  // Observability details
+  observability_namespace = module.observability.observability_namespace
 
-#   // Required client details to allow access and generate credentials and certificates for
-#   clients = [
-#     {
-#       namespace          = "cloud"
-#       user               = "cloud"
-#     }
-#   ]
+  // Required client details to allow access and generate credentials and certificates for
+  clients = [
+    {
+      namespace          = "cloud"
+      user               = "cloud"
+    }
+  ]
 
-#   // Certificate details for internal and ingress certificates
-#   cluster_issuer_name = module.cluster-issuer.cluster-issuer-name
-#   cloudflare_token    = var.cloudflare_token
-#   cloudflare_email    = var.cloudflare_email
-#   domain              = var.domain
+  // Certificate details for internal and ingress certificates
+  cluster_issuer_name = module.cluster-issuer.cluster-issuer-name
+  cloudflare_token    = var.cloudflare_token
+  cloudflare_email    = var.cloudflare_email
+  domain              = var.domain
 
-#   // Whitelisting Kubernetes API Endpoints in the Network Policy
-#   kubernetes_api_ip       = one(flatten(data.kubernetes_endpoints_v1.kubernetes_api_endpoint.subset[*].address[*].ip))
-#   kubernetes_api_protocol = one(flatten(data.kubernetes_endpoints_v1.kubernetes_api_endpoint.subset[*].port[*].protocol))
-#   kubernetes_api_port     = one(flatten(data.kubernetes_endpoints_v1.kubernetes_api_endpoint.subset[*].port[*].port))
+  // Whitelisting Kubernetes API Endpoints in the Network Policy
+  kubernetes_api_ip       = one(flatten(data.kubernetes_endpoints_v1.kubernetes_api_endpoint.subset[*].address[*].ip))
+  kubernetes_api_protocol = one(flatten(data.kubernetes_endpoints_v1.kubernetes_api_endpoint.subset[*].port[*].protocol))
+  kubernetes_api_port     = one(flatten(data.kubernetes_endpoints_v1.kubernetes_api_endpoint.subset[*].port[*].port))
 
-#   // Dependency on Garage Deployment  
-#   depends_on = [module.garage, module.observability, module.openbao]
-# }
+  // Dependency on Garage Deployment  
+  depends_on = [module.garage, module.observability, module.openbao]
+}
 
-# # Keycloak Cluster Deployment for Identity Solution
-# module "keycloak" {
-#   source = "git::https://github.com/necro-cloud/modules//modules/keycloak?ref=task/121/openbao-keycloak-credentials"
+# Keycloak Cluster Deployment for Identity Solution
+module "keycloak" {
+  source = "git::https://github.com/necro-cloud/modules//modules/keycloak?ref=task/121/openbao-keycloak-credentials"
 
-#   // Cluster Secret Store Details
-#   cluster_secret_store_name = module.openbao.cluster_secret_store_name
+  // Cluster Secret Store Details
+  cluster_secret_store_name = module.openbao.cluster_secret_store_name
   
-#   // PostgreSQL Database Details for database details
-#   cluster_issuer_name                        = module.cluster-issuer.cluster-issuer-name
-#   postgres_namespace                         = module.cnpg.namespace
-#   cluster_name                               = module.cnpg.cluster_name
-#   database_server_certificate_authority_name = module.cnpg.server-certificate-authority
-#   database_client_certificate_name           = "postgresql-keycloak-client-certificate"
-#   database_credentials                       = "credentials-keycloak"
+  // PostgreSQL Database Details for database details
+  cluster_issuer_name                        = module.cluster-issuer.cluster-issuer-name
+  postgres_namespace                         = module.cnpg.namespace
+  cluster_name                               = module.cnpg.cluster_name
+  database_server_certificate_authority_name = module.cnpg.server-certificate-authority
+  database_client_certificate_name           = "postgresql-keycloak-client-certificate"
+  database_credentials                       = "credentials-keycloak"
 
-#   // Certificate details for ingress
-#   cloudflare_token = var.cloudflare_token
-#   cloudflare_email = var.cloudflare_email
-#   domain           = var.domain
+  // Certificate details for ingress
+  cloudflare_token = var.cloudflare_token
+  cloudflare_email = var.cloudflare_email
+  domain           = var.domain
 
-#   // Observability details
-#   observability_namespace = module.observability.observability_namespace
+  // Observability details
+  observability_namespace = module.observability.observability_namespace
 
-#   // Realm Settings for auto configuration of required clients
-#   realm_settings = local.keycloak_realm_settings
+  // Realm Settings for auto configuration of required clients
+  realm_settings = local.keycloak_realm_settings
 
-#   // Dependency on CNPG PostgreSQL Deployment
-#   depends_on = [module.cnpg, module.observability, module.openbao]
-# }
+  // Dependency on CNPG PostgreSQL Deployment
+  depends_on = [module.cnpg, module.observability, module.openbao]
+}
 
-# # Valkey Deployment for In Memory Storage Solution
-# module "valkey" {
-#   source = "git::https://github.com/necro-cloud/modules//modules/valkey?ref=main"
+# Valkey Deployment for In Memory Storage Solution
+module "valkey" {
+  source = "git::https://github.com/necro-cloud/modules//modules/valkey?ref=main"
   
-#   // Cluster Secret Store Details
-#   cluster_secret_store_name = module.openbao.cluster_secret_store_name
+  // Cluster Secret Store Details
+  cluster_secret_store_name = module.openbao.cluster_secret_store_name
 
-#   // Certificate details for TLS Authentication
-#   cluster_issuer_name = module.cluster-issuer.cluster-issuer-name
+  // Certificate details for TLS Authentication
+  cluster_issuer_name = module.cluster-issuer.cluster-issuer-name
 
-#   // Granting required namespaces access to the Valkey
-#   access_namespaces = "cloud"
+  // Granting required namespaces access to the Valkey
+  access_namespaces = "cloud"
 
-#   // Observability details
-#   observability_namespace = module.observability.observability_namespace
+  // Observability details
+  observability_namespace = module.observability.observability_namespace
 
-#   depends_on = [module.observability, module.openbao]
-# }
+  depends_on = [module.observability, module.openbao]
+}
--- modules/keycloak/secrets.tf
+++ modules/keycloak/secrets.tf
@@ -20,7 +20,6 @@ resource "kubernetes_manifest" "database_credentials_sync" {
       target = {
         name = var.database_credentials
         template = {
-          # Using Opaque since the original secret was Opaque
           type = "Opaque"
           engineVersion = "v2"
         }
@@ -41,66 +40,163 @@ resource "kubernetes_manifest" "database_credentials_sync" {
   }
 }
 
-// Keycloak Credentials
-resource "random_password" "keycloak_password" {
-  length           = 20
-  lower            = true
-  numeric          = true
-  special          = true
-  override_special = "-_*/"
-  min_special      = 3
-}
-
-resource "kubernetes_secret" "keycloak_credentials" {
-  metadata {
-    name      = var.keycloak_credentials
-    namespace = kubernetes_namespace.namespace.metadata[0].name
-    labels = {
-      app       = var.app_name
-      component = "secret"
+// Password Generator for generating random passwords
+resource "kubernetes_manifest" "password_generator" {
+  manifest = {
+    apiVersion = "generators.external-secrets.io/v1alpha1"
+    kind       = "Password"
+    metadata = {
+      name      = "password-generator"
+      namespace = kubernetes_namespace.namespace.metadata[0].name
+    }
+    spec = {
+      length  = 20
+      digits  = 5
+      symbols = 0
+      noUpper = true
     }
   }
+}
 
-  data = {
-    KC_BOOTSTRAP_ADMIN_USERNAME = "keycloak.admin"
-    KC_BOOTSTRAP_ADMIN_PASSWORD = random_password.keycloak_password.result
+// Keycloak credentials configuration
+resource "kubernetes_manifest" "keycloak_credentials_sync" {
+  manifest = {
+    apiVersion = "external-secrets.io/v1"
+    kind       = "ExternalSecret"
+    metadata = {
+      name      = "keycloak-credentials"
+      namespace = kubernetes_namespace.namespace.metadata[0].name
+    }
+    spec = {
+      refreshInterval = "0"
+      target = {
+        name = "keycloak-credentials"
+        template = {
+          data = {
+            KC_BOOTSTRAP_ADMIN_USERNAME = "keycloak.admin"
+            KC_BOOTSTRAP_ADMIN_PASSWORD = "{{ .password }}"
+          }
+        }
+      }
+      dataFrom = [{
+        sourceRef = {
+          generatorRef = {
+            apiVersion = "generators.external-secrets.io/v1alpha1"
+            kind       = "Password"
+            name       = kubernetes_manifest.password_generator.object.metadata.name
+          }
+        }
+      }]
+    }
   }
+}
 
-  type = "Opaque"
+resource "kubernetes_manifest" "push_keycloak_credentials" {
+  manifest = {
+    apiVersion = "external-secrets.io/v1alpha1"
+    kind       = "PushSecret"
+    metadata = {
+      name      = "push-${kubernetes_manifest.keycloak_credentials_sync.object.spec.target.name}"
+      namespace = kubernetes_namespace.namespace.metadata[0].name
+    }
+    spec = {
+      refreshInterval = "1h"
+      deletionPolicy  = "None"
+      secretStoreRefs = [{
+        name = var.cluster_secret_store_name
+        kind = "ClusterSecretStore"
+      }]
+      selector = {
+        secret = {
+          name = kubernetes_manifest.keycloak_credentials_sync.object.spec.target.name
+        }
+      }
+      data = [
+        {
+          match = {
+            remoteRef = {
+              remoteKey = "${kubernetes_namespace.namespace.metadata[0].name}/credentials/ui/${kubernetes_manifest.keycloak_credentials_sync.object.spec.target.name}"
+            }
+          }
+        }
+      ]
+    }
+  }
+  depends_on = [kubernetes_manifest.keycloak_credentials_sync]
 }
 
 # Keycloak Realm Configuration
-resource "random_password" "tester_password" {
-  length           = 20
-  lower            = true
-  numeric          = true
-  special          = true
-  override_special = "-_*/"
-  min_special      = 3
-}
-
-resource "kubernetes_secret" "realm_secrets" {
-  metadata {
-    name      = "realm-secrets"
-    namespace = kubernetes_namespace.namespace.metadata[0].name
-
-    labels = {
-      app       = var.app_name
-      component = "secret"
+resource "kubernetes_manifest" "realm_secrets_sync" {
+  manifest = {
+    apiVersion = "external-secrets.io/v1"
+    kind       = "ExternalSecret"
+    metadata = {
+      name      = "realm-secrets"
+      namespace = kubernetes_namespace.namespace.metadata[0].name
+    }
+    spec = {
+      refreshInterval = "0"
+      target = {
+        name = "realm-secrets"
+        template = {
+          data = {
+            DISPLAY_NAME               = var.realm_settings["display_name"]
+            APPLICATION_NAME           = var.realm_settings["application_name"]
+            BASE_URL                   = var.realm_settings["base_url"]
+            VALID_LOGIN_REDIRECT_PATH  = var.realm_settings["valid_login_redirect_path"]
+            VALID_LOGOUT_REDIRECT_PATH = var.realm_settings["valid_logout_redirect_path"]
+            SMTP_HOST                  = var.realm_settings["smtp_host"]
+            SMTP_PORT                  = var.realm_settings["smtp_port"]
+            SMTP_MAIL                  = var.realm_settings["smtp_mail"]
+            SMTP_USERNAME              = var.realm_settings["smtp_username"]
+            SMTP_PASSWORD              = var.realm_settings["smtp_password"]
+            TESTER_SECRET              = "{{ .password }}"
+          }
+        }
+      }
+      dataFrom = [{
+        sourceRef = {
+          generatorRef = {
+            apiVersion = "generators.external-secrets.io/v1alpha1"
+            kind       = "Password"
+            name       = kubernetes_manifest.password_generator.object.metadata.name
+          }
+        }
+      }]
     }
   }
+}
 
-  data = {
-    DISPLAY_NAME               = var.realm_settings["display_name"]
-    APPLICATION_NAME           = var.realm_settings["application_name"]
-    BASE_URL                   = var.realm_settings["base_url"]
-    VALID_LOGIN_REDIRECT_PATH  = var.realm_settings["valid_login_redirect_path"]
-    VALID_LOGOUT_REDIRECT_PATH = var.realm_settings["valid_logout_redirect_path"]
-    SMTP_HOST                  = var.realm_settings["smtp_host"]
-    SMTP_PORT                  = var.realm_settings["smtp_port"]
-    SMTP_MAIL                  = var.realm_settings["smtp_mail"]
-    SMTP_USERNAME              = var.realm_settings["smtp_username"]
-    SMTP_PASSWORD              = var.realm_settings["smtp_password"]
-    TESTER_SECRET              = random_password.tester_password.result
+resource "kubernetes_manifest" "push_realm_secrets" {
+  manifest = {
+    apiVersion = "external-secrets.io/v1alpha1"
+    kind       = "PushSecret"
+    metadata = {
+      name      = "push-${kubernetes_manifest.realm_secrets_sync.object.spec.target.name}"
+      namespace = kubernetes_namespace.namespace.metadata[0].name
+    }
+    spec = {
+      refreshInterval = "1h"
+      deletionPolicy  = "None"
+      secretStoreRefs = [{
+        name = var.cluster_secret_store_name
+        kind = "ClusterSecretStore"
+      }]
+      selector = {
+        secret = {
+          name = kubernetes_manifest.realm_secrets_sync.object.spec.target.name
+        }
+      }
+      data = [
+        {
+          match = {
+            remoteRef = {
+              remoteKey = "${kubernetes_namespace.namespace.metadata[0].name}/infrastructure/${kubernetes_manifest.realm_secrets_sync.object.spec.target.name}"
+            }
+          }
+        }
+      ]
+    }
   }
+  depends_on = [kubernetes_manifest.realm_secrets_sync]
 }
