bf00887c6ca908d8caf2027ce37837d332cc7df0
khatrivarun <varun.h.khatri.dev@protonmail.com>
2026-04-08 15:30:26 +0530

    feat(ferretdb): update mongo express to use traefik

--- modules/ferretdb/ingress.tf
+++ modules/ferretdb/ingress.tf
@@ -8,18 +8,18 @@ resource "kubernetes_ingress_v1" "mongo_express_ingress" {
       component = "ingress"
     }
     annotations = {
-      "nginx.ingress.kubernetes.io/proxy-ssl-verify" : "on"
-      "nginx.ingress.kubernetes.io/proxy-ssl-secret" : "${kubernetes_namespace.namespace.metadata[0].name}/${kubernetes_manifest.mongo_express_internal_certificate.manifest.spec.secretName}"
-      "nginx.ingress.kubernetes.io/proxy-ssl-name" : "database.${kubernetes_namespace.namespace.metadata[0].name}.svc.cluster.local"
-      "nginx.ingress.kubernetes.io/backend-protocol" : "HTTPS"
-      "nginx.ingress.kubernetes.io/rewrite-target" : "/"
-      "nginx.ingress.kubernetes.io/proxy-body-size" : 0
-      "nginx.ingress.kubernetes.io/client-body-buffer-size" : "500M"
+      "traefik.ingress.kubernetes.io/router.middlewares" = join(",", [
+        "${kubernetes_namespace.namespace.metadata[0].name}-${kubernetes_manifest.middleware_rewrite.manifest.metadata.name}@kubernetescrd",
+        "${kubernetes_namespace.namespace.metadata[0].name}-${kubernetes_manifest.middleware_buffering.manifest.metadata.name}@kubernetescrd"
+      ])
+      "traefik.ingress.kubernetes.io/service.serverstransport" = "${kubernetes_namespace.namespace.metadata[0].name}-${kubernetes_manifest.transport.manifest.metadata.name}@kubernetescrd"
+      "traefik.ingress.kubernetes.io/router.tls" = "true"
+      "traefik.ingress.kubernetes.io/router.entrypoints" = "websecure"
     }
   }
 
   spec {
-    ingress_class_name = "nginx"
+    ingress_class_name = "traefik"
     tls {
       hosts       = ["${var.host_name}.${var.domain}"]
       secret_name = kubernetes_manifest.ingress_certificate.manifest.spec.secretName
@@ -41,4 +41,11 @@ resource "kubernetes_ingress_v1" "mongo_express_ingress" {
       }
     }
   }
+
+  
+  depends_on = [
+    kubernetes_manifest.middleware_rewrite,
+    kubernetes_manifest.middleware_buffering,
+    kubernetes_manifest.transport,
+  ]
 }
---
+++ modules/ferretdb/transport.tf
@@ -0,0 +1,19 @@
+// Server Transport Resource for HTTPS Backend Connections
+resource "kubernetes_manifest" "transport" {
+  manifest = {
+    apiVersion = "traefik.io/v1alpha1"
+    kind       = "ServersTransport"
+    metadata = {
+      name      = "transport"
+      namespace = kubernetes_namespace.namespace.metadata[0].name
+    }
+    spec = {
+      insecureSkipVerify = true
+      rootCAs = [
+        {
+          secret = kubernetes_manifest.mongo_express_internal_certificate.manifest.spec.secretName
+        }
+      ]
+    }
+  }
+}
