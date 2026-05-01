06356c9ec4a340814f83ff99972cb262a0d8f930
khatrivarun <varun.h.khatri.dev@protonmail.com>
2026-05-01 14:41:01 +0530

    feat(observability): using calico for network observability

--- modules/observability/network_observability.tf
+++ modules/observability/network_observability.tf
@@ -1,97 +1,91 @@
-resource "kubernetes_manifest" "network_observability" {
-	manifest = {
-    apiVersion = "flows.netobserv.io/v1beta2"
-    kind       = "FlowCollector"
-    metadata = {
-      // The operator expects this specific name
-      name = "cluster"
-    }
-    spec = {
-      namespace = kubernetes_namespace.namespace.metadata[0].name
-      
-      // "Direct" mode sends logs straight to OTel
-      deploymentModel = "Direct"
-
-      agent = {
-        type = "eBPF"
-        ebpf = {
-          // Required for "PacketDrop" to read kernel drop reasons
-          privileged = true
-          
-          // Enable drop detection and TCP round trips metrics
-          features = ["PacketDrop", "FlowRTT"] 
-          
-          // 25 means 1 in 25 packets
-          sampling           = 25
-          cacheActiveTimeout = "15s"
-          cacheMaxFlows      = 100000
-
-          // Ignore loopback traffic
-          excludeInterfaces  = ["lo"]
-
-          // Resource Constraints
-          resources = {
-            requests = {
-              cpu    = "50m"
-              memory = "100Mi"
-            }
-            limits = {
-              cpu    = "500m"
-              memory = "512Mi"
-            }
-          }
-        }
-      }
-
-      // Disable default stack for the netobserv instance
-      loki = {
-        enable = false
-      }
-      prometheus = {
-        querier = {
-          enable = false
-        }
-      }
-      consolePlugin = {
-        enable = false
-      }
-
-      //  Enrichment settings
-      processor = {
-        logTypes = "Flows"
-        metrics = {
-          // Disable agent-side metrics generation to save CPU
-          disableAlerts = ["NetObservLokiError", "NetObservNoFlows"] 
-        }
-      }
-
-      // Pushing metrics to the OTel Collector
-      exporters = [
-        {
-          type = "OpenTelemetry"
-          openTelemetry = {
-            targetHost = "otel-collector.${kubernetes_namespace.namespace.metadata[0].name}.svc.cluster.local"
-            targetPort = 4317
-            protocol   = "grpc"
-            
-            logs = {
-              enable = true
-              pushTimeInterval = "20s"
-              expiryTime       = "2m"
-            }
-            
-            metrics = {
-              enable = true
-            }
-            tls = {
-              enable = false
-              insecureSkipVerify = true
-            }
-          }
-        }
-      ]
-    }
+# Print out the network flow logs to stdout from Goldmane API
+resource "kubernetes_deployment" "goldmane_otel_adapter" {
+  metadata {
+    name      = "goldmane-otel-adapter"
+    namespace = "calico-system"
   }
 
-  depends_on = [ helm_release.otel_collector ]  
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "goldmane-otel-adapter"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "goldmane-otel-adapter"
+        }
+      }
+
+      spec {
+        container {
+          name    = "scraper"
+          image   = "alpine:latest"
+          command = ["/bin/sh", "-c"]
+          
+          args = [
+            <<-EOF
+            # Install jq and curl
+            apk add --no-cache jq curl
+
+            # Download and extract the grpcurl binary directly into our path
+            curl -sL https://github.com/fullstorydev/grpcurl/releases/download/v1.9.3/grpcurl_1.9.3_linux_x86_64.tar.gz | tar -xzf - -C /usr/local/bin grpcurl
+
+            # Run the stream and pipe it to jq
+            grpcurl -import-path /etc/proto -proto api.proto \
+              -cacert /etc/pki/tls/certs/tigera-ca-bundle.crt \
+              -cert /goldmane-key-pair/tls.crt \
+              -key /goldmane-key-pair/tls.key \
+              -d '{"start_time_gte": 0, "aggregation_interval": 5}' \
+              goldmane.calico-system.svc.cluster.local:7443 goldmane.Flows/Stream \
+              | jq --unbuffered -c '.'
+            EOF
+          ]
+
+          volume_mount {
+            name       = "proto-file"
+            mount_path = "/etc/proto"
+            read_only  = true
+          }
+
+          volume_mount {
+            name       = "goldmane-ca-bundle"
+            mount_path = "/etc/pki/tls/certs"
+            read_only  = true
+          }
+
+          volume_mount {
+            name       = "goldmane-key-pair"
+            mount_path = "/goldmane-key-pair"
+            read_only  = true
+          }
+        }
+
+        volume {
+          name = "proto-file"
+          config_map {
+            name = kubernetes_config_map.goldmane_api_proto.metadata[0].name
+          }
+        }
+
+        volume {
+          name = "goldmane-ca-bundle"
+          config_map {
+            name = "goldmane-ca-bundle"
+          }
+        }
+
+        volume {
+          name = "goldmane-key-pair"
+          secret {
+            secret_name = "goldmane-key-pair"
+          }
+        }
+      }
+    }
+  }
 }
